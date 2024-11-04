a1d872ebf6eddff9a551dcbd589508d77f38e4a7
AnHuy <92672519+danganhuy@users.noreply.github.com>
2024-11-04 15:51:56 +0700

    Exercise 4 finished

---
+++ src/LinearEquationResolver.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class LinearEquationResolver {
+    public static void main(String[] args) {
+        System.out.println("Linear Equation Resolver");
+        System.out.println("Given an equation as ax + b = c, please enter constants: ");
+
+        Scanner sc = new Scanner(System.in);
+        System.out.print("a: ");
+        double a = sc.nextDouble();
+
+        System.out.print("b: ");
+        double b = sc.nextDouble();
+
+        System.out.print("c: ");
+        double c = sc.nextDouble();
+
+        if (a != 0) {
+            double x = (c - b) / a;
+            System.out.println("Equation pass with x = " + x);
+        }
+        else {
+            if (b == c) {
+                System.out.println("The solution is all x");
+            } else {
+                System.out.println("The equation has no solution");
+            }
+        }
+
+    }
+}

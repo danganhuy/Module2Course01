15cdd75ac606d1ec2a650954e19520bfe5f43698
AnHuy <92672519+danganhuy@users.noreply.github.com>
2024-11-04 13:53:36 +0700

    first push

---
+++ src/Exercise1.java
@@ -0,0 +1,9 @@
+import java.util.Date;
+
+public class Exercise1 {
+    // [Thực hành] Hiển thị thời gian của hệ thống (v.2023)
+    public static void main(String[] args) {
+        Date now = new Date();
+        System.out.println("Current date: " + now);
+    }
+}

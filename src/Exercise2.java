b246911e3876f48ee0b8c52958d149123d21c95f
AnHuy <92672519+danganhuy@users.noreply.github.com>
2024-11-04 14:22:20 +0700

    Exercise 2 finished

---
+++ src/Exercise2.java
@@ -0,0 +1,18 @@
+public class Exercise2 {
+    // [Thực hành] Khai báo và sử dụng biến (v.2023)
+    public static void main(String[] args) {
+        int i = 5;
+        float f = 25.33f;
+        double d = 20.9;
+        boolean b = true;
+        char c = 'a';
+        String s = "Neon";
+
+        System.out.println("i = " + i);
+        System.out.println("f = " + f);
+        System.out.println("d = " + d);
+        System.out.println("b = " + b);
+        System.out.println("c = " + c);
+        System.out.println("s = " + s);
+    }
+}

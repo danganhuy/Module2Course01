b23f9fb220f2e53a18d659953bd2ddf8257656c5
AnHuy <92672519+danganhuy@users.noreply.github.com>
2024-11-05 09:39:28 +0700

    add commment

--- src/USDToVND.java
+++ src/USDToVND.java
@@ -1,6 +1,7 @@
 import java.util.Scanner;
 
 public class USDToVND {
+    // [Bài tập] Ứng dụng chuyển đổi tiền tệ
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
